ab79e1778ca5f9c5fc42146a27ee4d7ef00893d3
Simona Cinti <simona.cinti2@gmail.com>
2020-10-29 15:05:19 +0100

    random numbers

--- js/script.js
+++ js/script.js
@@ -6,4 +6,32 @@ $(document).ready(function(){
     //Todo 3 - Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
 
+    // Var 
+    var randomNumber = $('#random-number');
+    var collectionNumbers = [];
+    var minNumber = 1;
+    var maxNumber = 100;
+    var secondi = 5;
+
+    // Creazione numeri random da parte del computer
+
+    for ( i = 0; i < 5; i++){
+       var selectNumber = Math.floor(Math.random () *( maxNumber - minNumber + 1)) + minNumber;
+        collectionNumbers += ' ' + selectNumber;
+    };
+
+    console.log(collectionNumbers);
+    
+    // var timer = setInterval(function(){
+    //     if (secondi == 0){
+    //         console.log('scrivi i numeri!');
+    //         clearInterval(timer);
+
+    //     } else {
+    //         alert('numeri casuali');
+    //         secondi --;
+    //     }
+
+    // },1000);
+
 }); // <--- end doc ready
