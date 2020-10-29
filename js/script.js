27903161b423fffedf09a9ea33ef9041deb61fa8
Simona Cinti <simona.cinti2@gmail.com>
2020-10-29 14:20:10 +0100

    setup

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    
     console.log('hello js ', $);
+
+    //Todo 1- Un alert espone 5 numeri casuali diversi.
+    //Todo 2 - Dopo 30 secondi l'utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+    //Todo 3 - Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+
 
 }); // <--- end doc ready
